3d22787b172e53c9f27481e3cbd8dc382a2c7fa2
Елена Григорьева <gev_6484_gev@icloud.com>
2025-11-11 19:12:55 +0300

    init commit

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("welcome"))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		panic(err)
+	}
+}
